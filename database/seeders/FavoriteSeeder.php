c280a1b1d3765c36af70468b9a8ee416bd612797
AboYasser1 <mohammad@example.com>
2026-08-18 16:23:24 +0300

    feat: update backend with latest changes

--- database/seeders/FavoriteSeeder.php
+++ database/seeders/FavoriteSeeder.php
@@ -3,12 +3,32 @@
 namespace Database\Seeders;
 
 use App\Models\Favorite;
+use App\Models\Service;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class FavoriteSeeder extends Seeder
 {
     public function run(): void
     {
-        Favorite::factory()->count(15)->create();
+        $userIds = User::query()->pluck('id')->all();
+        $serviceIds = Service::query()->pluck('id')->all();
+
+        $pairs = [];
+        foreach ($userIds as $userId) {
+            foreach ($serviceIds as $serviceId) {
+                $pairs[] = [$userId, $serviceId];
+            }
+        }
+
+        shuffle($pairs);
+        $pairs = array_slice($pairs, 0, min(15, count($pairs)));
+
+        foreach ($pairs as [$userId, $serviceId]) {
+            Favorite::factory()->create([
+                'user_id' => $userId,
+                'service_id' => $serviceId,
+            ]);
+        }
     }
 }
