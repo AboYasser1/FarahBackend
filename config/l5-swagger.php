0f7a0a1f32ee7b33c60281252ba19b3699cc7593
AboYasser1 <mohammad@example.com>
2026-08-28 18:28:07 +0300

    fix: update swagger docs and api configuration

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -15,10 +15,7 @@ return [
                 'api' => 'api/documentation',
             ],
             'paths' => [
-                /*
-                 * Edit to include full URL in ui for assets
-                 */
-                'use_absolute_path' => env('L5_SWAGGER_USE_ABSOLUTE_PATH', true),
+                'use_absolute_path' => env('L5_SWAGGER_USE_ABSOLUTE_PATH', false),
 
                 /*
                 * Edit to set path where swagger ui assets should be stored
@@ -252,7 +249,7 @@ return [
          * Set this to `true` in development mode so that docs would be regenerated on each request
          * Set this to `false` to disable swagger generation on production
          */
-        'generate_always' => env('L5_SWAGGER_GENERATE_ALWAYS', false),
+        'generate_always' => env('L5_SWAGGER_GENERATE_ALWAYS', true),
 
         /*
          * Set this to `true` to generate a copy of documentation in yaml format
